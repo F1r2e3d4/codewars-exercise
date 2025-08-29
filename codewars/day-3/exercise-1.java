6174867ed53e680f6aba7d312deb980c1b8339f1
fredrick <fredrick8283@gmail.com>
2025-08-29 13:06:06 +0100

    highest and smallest in an array

---
+++ codewars/day-3/exercise-1.java
@@ -0,0 +1,16 @@
+// finding the largest ns smallest in an array
+
+class MinMax {
+    public static int[] minMax(int[] arr) {
+      int max = arr[0];
+      int min = arr[0];
+        for(int i = 1; i < arr.length; i ++){
+          if(arr [i] > max){
+            max = arr[i];
+          }if (arr[i] < min ){
+            min = arr[i];
+          }
+        }
+      return new int[]{ min, max };
+    }
+}
