1bb0a63d57fdf6c3badf7eab87d6474b2cb9bbd6
fredrick <fredrick8283@gmail.com>
2025-08-29 13:15:15 +0100

    adding contents of an array

---
+++ codewars/day-3/exercise-2.java
@@ -0,0 +1,13 @@
+// adding the contents of an array
+
+class SumArray {
+    public static int sumArray(int[] arr) {
+      int sum = 0;
+        for(int i = 0; i < arr.length; i ++){
+          sum += arr[i];
+        }
+      return sum;
+    }
+}
+
+
